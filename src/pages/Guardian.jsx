5a542a53d77a9ec336f017b86faddf484750efda
Negin-code <iamneginasem@gmail.com>
2025-06-24 20:48:30 -0700

    Update margin for Key Features heading in Guardian component

--- src/pages/Guardian.jsx
+++ src/pages/Guardian.jsx
@@ -1051,7 +1051,7 @@ const Guardian = () => {
         <section id="key-features" className="mt-16 overflow-hidden">
           <div className="flex flex-row items-center gap-2 mb-12">
             <div className="w-[3vh] sm:w-[3vh] md:w-[4vh] h-[2px] bg-[#493B32]"></div>
-            <h2 className="text-2xl sm:text-3xl font-tan text-[#493B32]">
+            <h2 className="text-2xl sm:text-3xl font-tan text-[#493B32] mt-10">
               Key Features
             </h2>
           </div>
