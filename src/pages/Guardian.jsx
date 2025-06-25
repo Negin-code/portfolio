2bb58da609e093bd4e4635d0fbd0573cb71776bd
Negin-code <iamneginasem@gmail.com>
2025-06-25 11:54:02 -0700

    Update margin in design goals section of Guardian component

--- src/pages/Guardian.jsx
+++ src/pages/Guardian.jsx
@@ -963,7 +963,7 @@ const Guardian = () => {
         </div>
 
         {/* Design goals section */}
-        <section id="design-goals" className="space-y-6 mt-16">
+        <section id="design-goals" className="space-y-6 mt-16 mr-15 sm:mr-0">
           <div className="flex flex-row items-center gap-2">
             <div className="w-[3vh] sm:w-[3vh] md:w-[4vh] h-[2px] bg-[#493B32]"></div>
             <h2 className="text-2xl sm:text-3xl font-tan text-[#493B32]">
