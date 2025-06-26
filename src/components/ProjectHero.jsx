dbedf86dda5ae35b7bd41a2fa28bffb0f89786f9
Negin-code <iamneginasem@gmail.com>
2025-06-25 20:18:37 -0700

    Refine margin adjustments for layout in ProjectHero component

--- src/components/ProjectHero.jsx
+++ src/components/ProjectHero.jsx
@@ -4,8 +4,8 @@ const ProjectHero = ({ title, image, imageAlt }) => {
   return (
     <>
       {/* Project Title */}
-      <div className="flex flex-row items-start justify-start gap-1 mb-8 pl-0 mt-8 sm:mt-0">
-        <div className="w-[4vh] sm:w-[4vh] md:w-[5vh] h-[3px] bg-gradient-to-r from-[#493B32] to-[#F75590] mt-4"></div>
+      <div className="flex flex-row items-start justify-start gap-1 mb-8 pl-0 mt-10 md:mt-0">
+        <div className="w-[4vh] sm:w-[4vh] md:w-[5vh] h-[3px] bg-gradient-to-r from-[#493B32] to-[#F75590]"></div>
         <h1 id="top" className="text-xl sm:text-4xl md:text-5xl leading-relaxed font-tan text-[#493B32]">
           {title}
         </h1>
@@ -13,7 +13,7 @@ const ProjectHero = ({ title, image, imageAlt }) => {
 
       {/* Project Hero Image Container */}
       <div className="w-full mb-8 sm:mb-12 lg:mb-16">
-        <div className="relative w-[calc(100%-1rem)] lg:w-full overflow-hidden bg-[#FFF7F2]">
+        <div className="relative w-full lg:w-full overflow-hidden bg-[#FFF7F2]">
           {/* Aspect ratio container */}
           <div className="relative w-full h-0 pb-[75%] sm:pb-[65%] md:pb-[55%] lg:pb-[45%]">
             <img
