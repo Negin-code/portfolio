f8b0db4b8ce01bcbe829c19e6c6b6480c041a4be
Negin-code <iamneginasem@gmail.com>
2025-07-19 11:50:12 -0700

    Fix comment formatting in About component for consistency

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -87,7 +87,7 @@ const About = () => {
 
   return (
     <main className="grid grid-cols-4 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-12 gap-4 min-h-screen bg-[#FFF7F2] pt-20 lg:pt-50 lg:px-0 md:px-8 relative overflow-hidden">
-      {/* Decorative elements - purely visual */}
+      {/* Decorative elements - purely visual*/}
       <div
         className="absolute left-17 md:left-20 top-0 bottom-8 h-[calc(100%-4rem)] w-[2px] bg-[#493b32]"
         aria-hidden="true"
